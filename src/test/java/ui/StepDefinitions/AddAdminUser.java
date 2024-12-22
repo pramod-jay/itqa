18182286dbb8107204cd78ba48ba63ca7f455ad1
lasantha117 <weerasinghehwlp.20@uom.lk>
2024-12-23 00:58:02 +0530

    Edited baseSteps

--- src/test/java/ui/StepDefinitions/AddAdminUser.java
+++ src/test/java/ui/StepDefinitions/AddAdminUser.java
@@ -17,7 +17,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import ui.Globals.BaseSteps;
+import ui.BaseSteps.BaseSteps;
 
 import java.time.Duration;
 
