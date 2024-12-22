aa592030f3d4e97e92808a33d552b727ff5dc05a
BuddimaDissanayake <124259099+BuddimaDissanayake@users.noreply.github.com>
2024-12-22 23:00:15 +0530

    created background add qualifications

--- src/test/java/ui/StepDefinitions/AddQualificationsSteps.java
+++ src/test/java/ui/StepDefinitions/AddQualificationsSteps.java
@@ -1,18 +1,23 @@
 package ui.StepDefinitions;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import ui.Globals.BaseSteps;
 
-public class AddQualificationsSteps {
+import java.time.Duration;
 
-    WebDriver driver;
-    WebDriverWait wait;
+public class AddQualificationsSteps extends BaseSteps {
+
+    @Before
+    public void setup() {
+        wait =  new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
 
     @When("I click the My info button")
     public void i_click_the_my_info_button() {
@@ -23,21 +28,42 @@ public class AddQualificationsSteps {
     }
     @Then("I should see the personal info screen")
     public void i_should_see_the_personal_info_screen() {
-        wait.until(ExpectedConditions.urlToBe("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewQualifications/empNumber/7"));
+        wait.until(ExpectedConditions.urlToBe("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/7"));
 
         String currentUrl = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrl, "https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewQualifications/empNumber/7");
+        Assert.assertEquals(currentUrl, "https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/7");
     }
     @When("I click the Qualifications button")
     public void i_click_the_qualifications_button() {
         WebElement qualificationsBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
-                "//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[6]"
+                "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[9]/a"
         )));
         qualificationsBtn.click();
     }
     @Then("I should see the Qualifications screen")
     public void i_should_see_the_qualifications_screen() {
+        wait.until(ExpectedConditions.urlToBe("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewQualifications/empNumber/7"));
 
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, "https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewQualifications/empNumber/7");
+    }
+
+    @When("I click work experience add button")
+    public void i_click_work_experience_add_button() {
+        WebElement addWorkExperienceBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+                "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[2]/div[1]/div/button"
+        )));
+        addWorkExperienceBtn.click();
+    }
+
+    @Then("I should see the work experience adding section")
+    public void i_should_see_the_work_experience_adding_section() {
+        WebElement sectionTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+                "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[2]/div[1]/h6"
+        )));
+        assert sectionTitle.isDisplayed() : "Section is not opened";
+
+        Assert.assertEquals(sectionTitle.getText(), "Add Work Experience");
     }
 
 }
