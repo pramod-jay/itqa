b9f3d6a4ea7e510e9c71a5f5a8b725bb8da23097
Pramod Jayathilaka <pramodjayathilaka@gmail.com>
2024-12-22 21:10:04 +0530

    test(ui): separate login steps

--- src/test/java/ui/StepDefinitions/ApplyLeaveSteps.java
+++ src/test/java/ui/StepDefinitions/ApplyLeaveSteps.java
@@ -6,7 +6,6 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
--- src/test/java/ui/StepDefinitions/LoginSteps.java
+++ src/test/java/ui/StepDefinitions/LoginSteps.java
@@ -4,7 +4,6 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
