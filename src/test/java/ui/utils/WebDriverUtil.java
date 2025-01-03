addd3eef45f58898f125acb96fd0748321d7d6e6
Pramod Jayathilaka <pramodjayathilaka@gmail.com>
2025-01-04 01:18:30 +0530

    removed driver options

--- src/test/java/ui/utils/WebDriverUtil.java
+++ src/test/java/ui/utils/WebDriverUtil.java
@@ -7,9 +7,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class WebDriverUtil {
     public static WebDriver getDriver() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // Runs Chrome in headless mode
-        options.addArguments("--no-sandbox"); // Disables sandboxing (for security)
-        options.addArguments("--disable-dev-shm-usage"); // Disables shared memory usage (avoids crashes)
         options.addArguments("--disable-web-security", "--ignore-certificate-errors");
         return new ChromeDriver(options);
     }
